461e735a738a2301f19113de1a74011e73132bf1
Ric Molina <135799169+ricmolina@users.noreply.github.com>
2025-12-28 03:22:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,13 +18,14 @@ name_on_order = st.text_input('Name on smoothie:')
 st.write('The name on your smoothie will be:', name_on_order)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-st.dataframe(data=my_dataframe) #, use_container_width=False)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect (
     'Choose up to 5 ingredients:'
     , my_dataframe
     , max_selections=5
 )
+
 
 if ingredients_list:
     ingredients_string = ''
