a3b6850e7827819c2e20207409879115995aacdd
Ric Molina <135799169+ricmolina@users.noreply.github.com>
2025-12-28 02:41:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.functions import when_matched
 
 
 # Write directly to the app
@@ -12,32 +11,16 @@ st.write(
   """
 )
 
-
-#session = get_active_session()
 cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-#editable_df = st.data_editor(my_dataframe)
-#st.dataframe(data=my_dataframe, use_container_width=True)
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response).json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
-if my_dataframe:
-  editable_df = st.data_editor(my_dataframe)
-  submitted = st.button('Submit')
-  if submitted:
-    og_dataset = session.table("smoothies.public.orders")
-    edited_dataset = session.create_dataframe(editable_df)
-    try:
-      og_dataset.merge(edited_dataset
-                       , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                       , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                      )
-      st.success("Someon clicked the button.", icon='👍')
-    except:
-        st.write('Something went wrong.')
-  else:
-      st.success('There are no pending orders right now', icon='👍')
+if ingredients_list:
+  ingredients_string= ''
+
+for fruit_chosen in ingredients_list:
+  ingredients_string += fruit_chosen + ' '
+  smoothiefroot_response = smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+  sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
