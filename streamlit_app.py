ffba1589609057deae06f292e1533fef837f78e9
Ric Molina <135799169+ricmolina@users.noreply.github.com>
2025-12-28 03:00:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,9 +13,9 @@ st.write(
 
 cnx=st.connection("snowflake")
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-
+st.stop
 
 if ingredients_list:
   ingredients_string= ''
