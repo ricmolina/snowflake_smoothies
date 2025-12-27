448389455b7ef35ab2399199f212fe89b2f78da7
Ric Molina <135799169+ricmolina@users.noreply.github.com>
2025-12-28 03:25:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,6 @@ ingredients_list = st.multiselect (
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
 
-
 if ingredients_list:
     ingredients_string = ''
 
