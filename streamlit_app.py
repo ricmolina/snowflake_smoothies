f8e43a98c767369498f7157fd5aa04859e218c09
Ric Molina <135799169+ricmolina@users.noreply.github.com>
2025-12-28 02:58:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write(
 
 cnx=st.connection("snowflake")
 session=cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME")==0).collect()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME")
 st.dataframe(data=my_dataframe, use_container_width=True)
 
 
